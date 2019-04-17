8a274983536f5b8022063304734360baa90716f4
Zhi Long Tan <zltan@fb.com>
2019-04-17 16:46:30 +0800

    made edits to accomodate list of ints, converted to list of strings

--- sexualreorg.py
+++ sexualreorg.py
@@ -2,11 +2,12 @@ import pandas as pd
 import xlwt
 import xlrd
 from xlutils.copy import copy as xl_copy
+import csv
 
-df = pd.read_csv('sexualscuba15.csv')
+df = pd.read_csv('16Apr8am_17Apr8pm.csv')
 timepd = df['Time']
-objidpd = df['objid']
-decisionpd = df['Decision']
+objidpd = df['Object Id']
+decisionpd = df['Decision Data']
 
 def output(filename, sheet):
     book = xlwt.Workbook()
@@ -21,11 +22,28 @@ def save_column_in_excel(fileName,sheetNumber,columnNumber,columnList):
     link_comments=0
     for each in columnList:
         if isinstance(each, str):
-            Sheet1.write(link_comments, columnNumber, each)
-            link_comments += 1
+            while len(each) > 30000:
+                Sheet1.write(link_comments, columnNumber, each[:30000])
+                each = each[30000:]
+                columnNumber += 1
+            else:
+                Sheet1.write(link_comments, columnNumber, each)
+                link_comments += 1
         elif isinstance(each, list):
-            Sheet1.write(link_comments, columnNumber, str(each)[1:-1])
-            link_comments += 1
+            # if its list of strings:
+            taglist = ", ".join([str(tag) for tag in each])
+            columnNumberedit = columnNumber
+            if len(taglist) > 30000:
+                while len(taglist) > 30000:
+                    Sheet1.write(link_comments, columnNumberedit, taglist[:30000])
+                    columnNumberedit += 1
+                    taglist = taglist[30000:]
+                Sheet1.write(link_comments, columnNumberedit, taglist[:30000])
+                link_comments += 1
+            else:
+                Sheet1.write(link_comments, columnNumber, taglist)
+                link_comments += 1
+
         elif isinstance(each, int):
             Sheet1.write(link_comments, columnNumber, each)
             link_comments += 1
@@ -38,12 +56,8 @@ decision = []
 for each in timepd:
     time.append(each)
 
-print (time)
-
 for each in objidpd:
     objid.append(each)
-
-print (time)
 
 for each in decisionpd:
     decision.append(each)
@@ -68,10 +82,8 @@ for tree in decisionnew:
             result[tag]["id"] = list(set(result[tag]["id"]))
     count += 1
 
-print (result)
-
-fileName = "brendon"
-sheetName = "15April2019"
+fileName = "16Apr8am_17Apr8pm_processed"
+sheetName = "test"
 
 output(fileName + ".xls",sheetName)
 # open existing workbook
@@ -86,3 +98,10 @@ sheet1_col3_jobs = ['objids'] + [value['id'] for key, value in result.items()]
 save_column_in_excel(fileName + ".xls", 0, 0, sheet1col1TagNames)
 save_column_in_excel(fileName + ".xls", 0, 1, sheet1_col2_num3)
 save_column_in_excel(fileName + ".xls", 0, 2, sheet1_col3_jobs)
+
+# rows = zip(sheet1col1TagNames, sheet1_col2_num3, sheet1_col3_jobs)
+
+# with open(fileName+'.csv', "w") as f:
+#     writer = csv.writer(f)
+#     for row in sheet1col1TagNames:
+#         writer.writerow(row)
